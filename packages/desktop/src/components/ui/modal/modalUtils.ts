c84c7e338a7e774827de309a31a48115be914bd0
matej <matej@krecmer.eu>
2026-05-19 15:27:26 +0200

    refactor(modal): optimize body scroll locking logic and save previous styles

--- packages/desktop/src/components/ui/modal/modalUtils.ts
+++ packages/desktop/src/components/ui/modal/modalUtils.ts
@@ -2,26 +2,30 @@ const FOCUSABLE_SELECTOR =
   'button:not([disabled]), [href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"])';
 
 let modalOpenCount = 0;
+let savedOverflow = "";
+let savedPaddingRight = "";
 
 export function getFocusableSelector() {
   return FOCUSABLE_SELECTOR;
 }
 
 export function lockBodyScroll() {
-  modalOpenCount += 1;
-  const previousOverflow = document.body.style.overflow;
-  const previousPaddingRight = document.body.style.paddingRight;
-  const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
-  document.body.style.overflow = "hidden";
-  if (scrollbarWidth > 0) {
-    document.body.style.paddingRight = `${scrollbarWidth}px`;
+  if (modalOpenCount === 0) {
+    savedOverflow = document.body.style.overflow;
+    savedPaddingRight = document.body.style.paddingRight;
+    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+    if (scrollbarWidth > 0) {
+      document.body.style.paddingRight = `${scrollbarWidth}px`;
+    }
   }
+  modalOpenCount += 1;
+  document.body.style.overflow = "hidden";
 
   return () => {
     modalOpenCount -= 1;
     if (modalOpenCount <= 0) {
-      document.body.style.overflow = previousOverflow;
-      document.body.style.paddingRight = previousPaddingRight;
+      document.body.style.overflow = savedOverflow;
+      document.body.style.paddingRight = savedPaddingRight;
       modalOpenCount = 0;
     }
   };
